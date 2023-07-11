899a7aa2c68de7ea04ade5cb9e58a1580f1301ce
Lynn Kamau <98352438+LynnKamau@users.noreply.github.com>
2023-07-11 22:45:47 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-Save Module
-"""
+"""add item"""
+
+
 import json
+import sys
+import os.path
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj, filename):
-    """write an object to a text file
-    args:
-        my_obj: object to manipulate
-    filename:
-        filename: text file
-    return:
-        object
-    """
-
-    with open(filename, "w", encoding="utf-8") as f:
-        written = json.dump(my_obj, f)
-    return written
+filename = "add_item.json"
+if os.path.isfile(filename):
+    obj = load_from_json_file(filename)
+else:
+    obj = []
+obj.extend(sys.argv[1:])
+save_to_json_file(obj, filename)
